8030301a3a9f2edb97c95df4b26bb96be8239c5d
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-13 22:41:28 -0400

    calculator add

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,71 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    if (arr.length == 0) {
+        return 0;
+
+    } else if (arr.length == 1) {
+        return arr[0];
+    } else {
+        let i = arr.length;
+        let number = 0;
+        while (i > 0) {
+            number += arr[i - 1]
+
+
+            i--;
+        }
+
+        return number;
+    }
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+
+    let arrLength = arr.length;
+    let number = 1;
+    while (arrLength > 0) {
+        number *= arr[arrLength - 1];
+        arrLength--;
+    }
+    return number;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+
+
+    return Math.pow(a, b);;
+
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+    if (a == 0) {
+        return 1;
+    } else if (a == 1) {
+        return 1;
+    } else {
+
+
+
+        return factorial(a - 1) * a;
+
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
