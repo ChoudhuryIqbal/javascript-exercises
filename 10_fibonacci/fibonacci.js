86ae15a168899935712622f09e06bc9c3661bec2
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-14 00:38:46 -0400

    Add Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+    if (parseInt(number) < 0) {
+        return "OOPS";
+
+    } else {
+        number = parseInt(number)
+        let n1 = 0;
+        let n2 = 1;
+        let nextTerm = 0;
+        for (let i = 2; i <= number; i++) {
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm
+        }
+
+        return n2;
+
+    }
 
 };
 
