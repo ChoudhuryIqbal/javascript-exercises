d430cc482850d08318c21991c530f0a05fa1a809
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2022-06-14 00:00:10 -0400

    Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    str = str.replace(/\s/g, '');
+    str = str.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    arrayStr = str.split("")
+    reversedStr = str.split("").reverse().join();
+    return (arrayStr == reversedStr)
 
 };
 
